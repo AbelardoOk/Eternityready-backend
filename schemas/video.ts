a9343c7564141bdc7f712266b9b3ddaac9e30da1
AbelardoOk <abelardoemailprivado@gmail.com>
2025-08-05 10:13:08 -0400

    Feat: Youtube video data sync from URL

--- schemas/video.ts
+++ schemas/video.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import { list } from "@keystone-6/core";
 import {
   text,
@@ -9,6 +10,42 @@ import {
   image,
 } from "@keystone-6/core/fields";
 import { allowAll } from "@keystone-6/core/access";
+import axios from "axios";
+
+function getYouTubeVideoId(url: string): string | null {
+  const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
+  const match = url.match(regExp);
+  return match && match[2].length === 11 ? match[2] : null;
+}
+
+async function fetchYoutubeVideoDetails(videoId: string) {
+  const apiKey = process.env.YOUTUBE_API_KEY;
+  if (!apiKey) throw new Error("Youtube Api Key not set in .env file");
+
+  const url = `https://www.googleapis.com/youtube/v3/videos?id=${videoId}&key=${apiKey}&part=snippet`;
+  const embed = `<iframe width="560" height="315" src="https://www.youtube.com/embed/${videoId}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`;
+  try {
+    const { data } = await axios.get(url);
+    if (data.items && data.items.length > 0) {
+      const snippet = data.items[0].snippet;
+      return {
+        title: snippet.title,
+        description: snippet.description,
+        thumbnailUrl:
+          snippet.thumbnails.default?.url ||
+          snippet.thumbnails.high?.url ||
+          snippet.thumbnails.maxres?.url,
+        channelTitle: snippet.channelTitle,
+        createdAt: snippet.publishedAt,
+        embedCode: embed,
+      };
+    }
+    return null;
+  } catch (error) {
+    console.error("Error finding data from YT:", error);
+    return null;
+  }
+}
 
 export const Video = list({
   access: {
@@ -26,8 +63,11 @@ export const Video = list({
     },
   },
   fields: {
-    title: text({ validation: { isRequired: true } }),
-    description: text({ ui: { displayMode: "textarea" } }),
+    title: text({ ui: { itemView: { fieldMode: "read" } } }),
+    description: text({
+      ui: { displayMode: "textarea" },
+      db: { nativeType: "Text", isNullable: true },
+    }),
     sourceType: select({
       options: [
         { label: "YouTube", value: "youtube" },
@@ -39,16 +79,85 @@ export const Video = list({
     }),
     youtubeUrl: text({
       ui: { description: "URL from youtube video" },
+      db: { nativeType: "Text", isNullable: true },
+    }),
+    videoId: text({
+      label: "ID do Vídeo (Automático)",
+      isIndexed: "unique",
+      ui: { itemView: { fieldMode: "read" } },
     }),
     embedCode: text({
       ui: { displayMode: "textarea", description: "<iframe> code" },
+      db: { nativeType: "Text", isNullable: true },
     }),
     uploadedFile: file({ storage: "video_files" }),
     thumbnail: image({ storage: "thumbnails" }),
     isPublic: checkbox({ defaultValue: true, label: "Public" }),
     categories: relationship({ ref: "Category.videos", many: true }),
-    // author: relationship({ ref: "User.videos" }),
-    author: text({ validation: { isRequired: true } }),
+    author: text({ validation: { isRequired: false } }),
     createdAt: timestamp({ defaultValue: { kind: "now" } }),
+  },
+  hooks: {
+    resolveInput: async ({ resolvedData, operation, item, context }) => {
+      const { images } = context;
+      const { sourceType, youtubeUrl } = resolvedData;
+
+      if (sourceType === "youtube" && youtubeUrl) {
+        if (operation === "update" && youtubeUrl === item?.youtubeUrl) {
+          return resolvedData;
+        }
+
+        const videoId = getYouTubeVideoId(youtubeUrl);
+        if (!videoId) return resolvedData;
+
+        const existing = await context.prisma.video.findUnique({
+          where: { videoId },
+        });
+
+        if (existing) {
+          console.warn(`Vídeo with ID ${videoId} Already exists.`);
+          return existing;
+        }
+        const videoDetails = await fetchYoutubeVideoDetails(videoId);
+        if (videoDetails?.thumbnailUrl) {
+          try {
+            const response = await axios.get(videoDetails.thumbnailUrl, {
+              responseType: "stream",
+            });
+            if (response.status === 200) {
+              const filename = `youtube-thumbnail-${videoId}${path.extname(
+                videoDetails.thumbnailUrl
+              )}`;
+              // const streamable = response.data as NodeJS.ReadableStream;
+
+              // Envia o stream para o adaptador de armazenamento de arquivos do Keystone
+              const imageData = await context
+                .images("thumbnails")
+                .getDataFromStream(response.data, filename);
+
+              return {
+                ...resolvedData,
+                videoId,
+                title: resolvedData.title || videoDetails.title,
+                description:
+                  resolvedData.description || videoDetails.description,
+                thumbnail: imageData,
+                author: resolvedData.channelTitle || videoDetails.channelTitle,
+                createdAt: videoDetails.createdAt || resolvedData.createdAt,
+                embedCode: videoDetails.embedCode || resolvedData.embedCode,
+              };
+            } else {
+              console.error("Erro ao baixar a thumbnail:", response.status);
+            }
+          } catch (error) {
+            console.error("Erro ao processar a thumbnail:", error);
+          }
+        } else {
+          console.warn("Nenhuma URL de thumbnail encontrada para este vídeo.");
+        }
+      }
+
+      return resolvedData;
+    },
   },
 });
